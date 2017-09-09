6f0fcc384b46cf9f6dbf173c10a7c1589eaa5fc3
vikashanumegowda <hanmego@usc.edu>
2017-09-08 21:54:41 -0700

    NQeens working fine. Obstructing trees to be implemented

---
+++ bfs.py
@@ -0,0 +1,101 @@
+from collections import deque
+import time
+
+if __name__ == "__main__":
+    f = open("input.txt", "r")
+    f.readline()  # for the first line retrieval
+    n = int(f.readline().strip())  # Size of square board
+    p = int(f.readline().strip())  # Number of Queens
+
+    # n vs p check (TO BE DONE)
+    start = time.time()
+    q1 = deque()
+
+    matrix1 = [[0 for x in range(n)] for x in range(n)]
+
+    for x in range(n):  # first column possible positions of Q
+        q1.insert(0, tuple([0, x, [[y for y in xx] for xx in matrix1], 0]))
+
+    nqueens1 = 0
+
+
+    def print_matrix(matrix):
+        for eachrow in matrix:
+            for eachcolumn in eachrow:
+                print(eachcolumn, end=' ')
+            print()
+
+
+    def dfs(q, p):
+
+        while len(q) > 0:
+            row, col, matrix, nqueens1 = q.pop()
+
+            matrix[row][col] = 'Q'
+            nqueens1 += 1
+
+            for x in range(n):  # to sides
+                if type(matrix[row][x]) != str and x != col:
+                    matrix[row][x] += 1
+                if type(matrix[x][col]) != str and x != row:
+                    matrix[x][col] += 1
+
+            x, y = col, row
+
+            while n > x >= 0 and n > y >= 0:  # to lower-left end
+                if type(matrix[y][x]) == int:
+                    matrix[y][x] += 1
+                y += 1
+                x -= 1
+
+            x, y = col, row
+            while n > x >= 0 and n > y >= 0:  # to upper-right end
+                if type(matrix[y][x]) == int:
+                    matrix[y][x] += 1
+                y -= 1
+                x += 1
+
+            x, y = col, row
+            while n > x >= 0 and n > y >= 0:  # to upper-left end
+                if type(matrix[y][x]) == int:
+                    matrix[y][x] += 1
+                y -= 1
+                x -= 1
+
+            x, y = col, row
+            while n > x >= 0 and n > y >= 0:  # to lower-right end
+                if type(matrix[y][x]) == int:
+                    matrix[y][x] += 1
+                y += 1
+                x += 1
+
+            if row == n - 1:
+                if nqueens1 != p:
+                    print("No solutions")
+                    continue
+                else:
+                    print("Success")
+                    print_matrix(matrix)
+                    return nqueens1
+
+            for x in range(n):  # append all 0 valued cells into Q
+                if matrix[row + 1][x] == 0:
+                    q.insert(0, tuple([row + 1, x, [[y for y in xx] for xx in matrix], nqueens1]))
+
+        return -1
+
+
+    # print_matrix(matrix1)
+    numberq = dfs(q1, p)
+
+    if numberq == -1:
+        print("Failure")
+
+    end = time.time()
+
+    print("time : " + str(end - start))
+    # for y in matrix:
+    #     for x in y:
+    #         print(x,end=' ')
+    #     print()
+    # print(str(matrix)+"\n"+str(q))
--- dfs.py
+++ dfs.py
@@ -1,4 +1,5 @@
 from collections import deque
+import time
 
 if __name__ == "__main__":
     f = open("input.txt", "r")
@@ -7,13 +8,13 @@ if __name__ == "__main__":
     p = int(f.readline().strip())  # Number of Queens
 
     # n vs p check (TO BE DONE)
-
+    start = time.time()
     q1 = deque()
 
     matrix1 = [[0 for x in range(n)] for x in range(n)]
 
     for x in range(n):  # first column possible positions of Q
-        q1.append(tuple([0, x, matrix1, 0]))
+        q1.append(tuple([0, x, [[y for y in xx] for xx in matrix1], 0]))
 
     nqueens1 = 0
 
@@ -25,88 +26,82 @@ if __name__ == "__main__":
             print()
 
 
-    def dfs(q):
+    def dfs(q, p):
 
-        while(len(q)>0):
-            node = q.pop()
+        while len(q) > 0:
+            row, col, matrix, nqueens1 = q.pop()
 
-            row = node[0]
-            col = node[1]
-            matrix = node[2]
-            nqueens1 = node[3]
-            print(row, col)
             matrix[row][col] = 'Q'
             nqueens1 += 1
-            if nqueens1 == p:
-                print("Success")
-                return nqueens1
 
             for x in range(n):  # to sides
                 if type(matrix[row][x]) != str and x != col:
                     matrix[row][x] += 1
-
-            for x in range(n):  # to top-bottom
                 if type(matrix[x][col]) != str and x != row:
                     matrix[x][col] += 1
 
-            x = col
-            y = row
+            x, y = col, row
+
             while n > x >= 0 and n > y >= 0:  # to lower-left end
                 if type(matrix[y][x]) == int:
                     matrix[y][x] += 1
                 y += 1
                 x -= 1
 
-            x = col
-            y = row
+            x, y = col, row
             while n > x >= 0 and n > y >= 0:  # to upper-right end
                 if type(matrix[y][x]) == int:
                     matrix[y][x] += 1
                 y -= 1
                 x += 1
 
-            x = col
-            y = row
+            x, y = col, row
             while n > x >= 0 and n > y >= 0:  # to upper-left end
                 if type(matrix[y][x]) == int:
                     matrix[y][x] += 1
                 y -= 1
                 x -= 1
 
-            x = col
-            y = row
+            x, y = col, row
             while n > x >= 0 and n > y >= 0:  # to lower-right end
                 if type(matrix[y][x]) == int:
                     matrix[y][x] += 1
                 y += 1
                 x += 1
 
-            print_matrix(matrix)
+            if row == n - 1:
+                if nqueens1 != p:
+                    print("No solutions")
+                    continue
+                else:
+                    print("Success")
+                    print_matrix(matrix)
+                    return nqueens1
 
             propagation_possible = 0
 
             for x in range(n):  # append all 0 valued cells into Q
                 if matrix[row + 1][x] == 0:
                     propagation_possible += 1
-                    q.append(tuple([row + 1, x, matrix, nqueens1+1]))
+                    q.append(tuple([row + 1, x, [[y for y in xx] for xx in matrix], nqueens1]))
 
             if propagation_possible == 0:
                 continue
-
-            print(q)
-            if row == n - 1:
-                if nqueens1 != p:
-                    print("No solutions")
-                    continue
+                # print(nqueens1)
+        return -1
 
 
-    print_matrix(matrix1)
-    numberq = dfs(q1)
+    # print_matrix(matrix1)
+    numberq = dfs(q1, p)
 
     if numberq == -1:
         print("Failure")
-        # for y in matrix:
-        #     for x in y:
-        #         print(x,end=' ')
-        #     print()
-        # print(str(matrix)+"\n"+str(q))
+
+    end = time.time()
+
+    print("time : " + str(end - start))
+    # for y in matrix:
+    #     for x in y:
+    #         print(x,end=' ')
+    #     print()
+    # print(str(matrix)+"\n"+str(q))
