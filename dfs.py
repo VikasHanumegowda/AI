815b6045693c957e3fe3478733202e9a4d0e626b
vikashanumegowda <hanmego@usc.edu>
2017-09-12 14:43:20 -0700

    Rotate matrix 90° 4 times implemented for finding solutions not starting with first row. Many bugs need to be fixed regarding trees.

--- dfs.py
+++ dfs.py
@@ -20,8 +20,19 @@ if __name__ == "__main__":
                     print(1, end=' ')
                 else:
                     print(0, end=' ')
-                    # print(eachcolumn, end=' ')
             print()
+
+
+    def rotate(matrix, degree):
+        if abs(degree) not in [0, 90, 180, 270, 360]:
+            # raise error or just return nothing or original
+            pass
+        if degree == 0:
+            return list(matrix)
+        elif degree > 0:
+            return rotate(list(zip(*matrix[::-1])), degree - 90)
+        else:
+            return rotate(list(zip(*matrix)[::-1]), degree + 90)
 
 
     def mark_conflicts(row, col, matrix):
@@ -30,26 +41,22 @@ if __name__ == "__main__":
                 break
             if type(matrix[row][x]) != str and x != col:  # to sides
                 matrix[row][x] += 1
-        # for x in range(col,n)
         for x in range(col, n):  # rightside
             if matrix[row][x] == 'T':
                 break
-            if type(matrix[row][x]) != str and x != col:  # top-bottom
+            if type(matrix[row][x]) != str and x != col:
                 matrix[row][x] += 1
-
         for x in range(row, -1, -1):  # top
             if matrix[x][col] == 'T':
                 break
-            if type(matrix[x][col]) != str and x != row:  # to sides
+            if type(matrix[x][col]) != str and x != row:
                 matrix[x][col] += 1
-        # for x in range(col,n)
         for x in range(row, n):  # bottom
             if matrix[x][col] == 'T':
                 break
-            if type(matrix[x][col]) != str and x != row:  # top-bottom
+            if type(matrix[x][col]) != str and x != row:
                 matrix[x][col] += 1
         x, y = col, row
-
         while n > x >= 0 and n > y >= 0:  # to lower-left end
             if matrix[y][x] == 'T':
                 break
@@ -57,7 +64,6 @@ if __name__ == "__main__":
                 matrix[y][x] += 1
             y += 1
             x -= 1
-
         x, y = col, row
         while n > x >= 0 and n > y >= 0:  # to upper-right end
             if matrix[y][x] == 'T':
@@ -66,7 +72,6 @@ if __name__ == "__main__":
                 matrix[y][x] += 1
             y -= 1
             x += 1
-
         x, y = col, row
         while n > x >= 0 and n > y >= 0:  # to upper-left end
             if matrix[y][x] == 'T':
@@ -75,7 +80,6 @@ if __name__ == "__main__":
                 matrix[y][x] += 1
             y -= 1
             x -= 1
-
         x, y = col, row
         while n > x >= 0 and n > y >= 0:  # to lower-right end
             if matrix[y][x] == 'T':
@@ -84,7 +88,6 @@ if __name__ == "__main__":
                 matrix[y][x] += 1
             y += 1
             x += 1
-
         return deepcopy(matrix)
 
 
@@ -104,25 +107,17 @@ if __name__ == "__main__":
 
     def dfs(q, p):
         if p == 0:
-            return p
+            return p, deepcopy(q.pop()[2])
         queens_ret = 0
         while len(q) > 0:
             row, col, matrix, nqueens1 = q.pop()
-
             if matrix[row][col] == 0:
                 matrix[row][col] = 'Q'
                 nqueens1 += 1
-
                 matrix = deepcopy(mark_conflicts(row, col, matrix))
-                print()
-
                 if nqueens1 == p:
-                    print("OK")
-                    print_output(matrix)
-                    return nqueens1
-
+                    return nqueens1, deepcopy(matrix)
                 q = deepcopy(find_possible_children(row, n, matrix, q, nqueens1))
-
                 if row == n - 1:
                     if nqueens1 < p:
                         for x in range(n):
@@ -131,78 +126,54 @@ if __name__ == "__main__":
                                     nqueens1 += 1
                                     matrix[x][y] = 'Q'
                                     matrix = deepcopy(mark_conflicts(x, y, matrix))
-                                    # print_matrix(matrix)
-                                    # print([x[:2] for x in q])
-                                    # print
                                     if nqueens1 == p:
-                                        print("OK")
-                                        print_output(matrix)
-                                        return nqueens1
+                                        return nqueens1, matrix
                                     q = deepcopy(find_possible_children(row, n, matrix, q, nqueens1))
-
                 queens_ret = nqueens1
-                # print_matrix(matrix)
-                # print([x[:2] for x in q])
-                # print()
-
+            print_output(matrix)
+            print()
             if queens_ret == p:
-                print("OK")
-                print_output(matrix)
-                return queens_ret
-                # for x in matrixreturn -1
-            # print_matrix(matrix)
-            # print([x[:2] for x in q])
-            # print()
-        return -1
+                return queens_ret, matrix
+        return -1, matrix
 
-
-    def rotate(matrix, degree):
-        if abs(degree) not in [0, 90, 180, 270, 360]:
-            pass
-        # raise error or just return nothing or original
-        if degree == 0:
-            return matrix
-        elif degree > 0:
-            return rotate(zip(*matrix[::-1]), degree - 90)
-        else:
-            return rotate(zip(*matrix)[::-1], degree + 90)
 
     f = open("input.txt", "r")
     f.readline()  # for the first line retrieval
     n = int(f.readline().strip())  # Size of square board
     p = int(f.readline().strip())  # Number of Queens
-    # print(n)
-    # n vs p check (TO BE DONE)
-    start = time.time()
-    q1 = deque()
-
-    # matrix1 = [[0 for x in range(n)] for x in range(n)]
     matrix1 = []
     for x in range(n):
         matrix1.append([int(x) if x == '0' else 'T' for x in f.readline().strip()])
-    # for eachrow in matrix1:
-    #     for eachcolumn in eachrow:
-    #         print(eachcolumn, end=' ')
-    #     print()
-    # for x in range(4):
+    start = time.time()
+    q1 = deque()
     numberq = []
-    for x in range(4):
+    found = 0
+    for x in range(2):
         matrix1 = [[i for i in j] for j in rotate(matrix1, 90)]
-        print_matrix(matrix1)
-        print(x+1)
-        for x in range(n):  # first column possible positions of Q
-            if matrix1[0][x] == 0:
-                q1.append(tuple([0, x, deepcopy(matrix1), 0]))
-
+        for xx in range(n):  # first column possible positions of Q
+            if matrix1[0][xx] == 0:
+                q1.append(tuple([0, xx, deepcopy(matrix1), 0]))
         nqueens1 = 0
+        ret_nq, matrix = dfs(q1, p)
+        if x == 0:
+            print(x + 1)
+            matrix = deepcopy(rotate(matrix, 270))
 
-        numberq.append(dfs(q1, p))
-        if any([x != -1 for x in numberq]):
+        elif x == 1:
+            print(x + 1)
+            matrix = deepcopy(rotate(matrix, 180))
+
+        if ret_nq == p:
             print("OK")
-        # print(numberq)
-        if numberq == -1:
-            print("Failure")
+            print_output(matrix)
+            found = 1
+            break
+            print()
 
+        numberq.append([ret_nq, matrix])
+
+    if found == 0:
+        if all([xx[0] == -1 for xx in numberq]):
+            print("FAIL")
     end = time.time()
-
     print("time : " + str(end - start))
--- dfs_bfs.py
+++ dfs_bfs.py
@@ -1,7 +1,6 @@
 from collections import deque
 from copy import deepcopy
 import time
-import copy
 
 if __name__ == "__main__":
 
@@ -21,48 +20,42 @@ if __name__ == "__main__":
                     print(1, end=' ')
                 else:
                     print(0, end=' ')
-                    # print(eachcolumn, end=' ')
             print()
 
 
     def rotate(matrix, degree):
         if abs(degree) not in [0, 90, 180, 270, 360]:
+            # raise error or just return nothing or original
             pass
-        # raise error or just return nothing or original
         if degree == 0:
-            return matrix
+            return list(matrix)
         elif degree > 0:
-            return rotate(zip(*matrix[::-1]), degree - 90)
+            return rotate(list(zip(*matrix[::-1])), degree - 90)
         else:
-            return rotate(zip(*matrix)[::-1], degree + 90)
+            return rotate(list(zip(*matrix)[::-1]), degree + 90)
 
 
     def mark_conflicts(row, col, matrix):
-
         for x in range(col, -1, -1):  # leftside
             if matrix[row][x] == 'T':
                 break
             if type(matrix[row][x]) != str and x != col:  # to sides
                 matrix[row][x] += 1
-
         for x in range(col, n):  # rightside
             if matrix[row][x] == 'T':
                 break
             if type(matrix[row][x]) != str and x != col:
                 matrix[row][x] += 1
-
         for x in range(row, -1, -1):  # top
             if matrix[x][col] == 'T':
                 break
             if type(matrix[x][col]) != str and x != row:
                 matrix[x][col] += 1
-
         for x in range(row, n):  # bottom
             if matrix[x][col] == 'T':
                 break
             if type(matrix[x][col]) != str and x != row:
                 matrix[x][col] += 1
-
         x, y = col, row
         while n > x >= 0 and n > y >= 0:  # to lower-left end
             if matrix[y][x] == 'T':
@@ -71,7 +64,6 @@ if __name__ == "__main__":
                 matrix[y][x] += 1
             y += 1
             x -= 1
-
         x, y = col, row
         while n > x >= 0 and n > y >= 0:  # to upper-right end
             if matrix[y][x] == 'T':
@@ -80,7 +72,6 @@ if __name__ == "__main__":
                 matrix[y][x] += 1
             y -= 1
             x += 1
-
         x, y = col, row
         while n > x >= 0 and n > y >= 0:  # to upper-left end
             if matrix[y][x] == 'T':
@@ -89,7 +80,6 @@ if __name__ == "__main__":
                 matrix[y][x] += 1
             y -= 1
             x -= 1
-
         x, y = col, row
         while n > x >= 0 and n > y >= 0:  # to lower-right end
             if matrix[y][x] == 'T':
@@ -98,39 +88,24 @@ if __name__ == "__main__":
                 matrix[y][x] += 1
             y += 1
             x += 1
-
         return deepcopy(matrix)
 
 
-    def find_possible_children(row, n, matrix_for_children, q, nqueens1):
-        for x in reversed(range(n)):  # append all 0 valued cells into Q
-            if matrix_for_children[row][x] == 0:
-                q.insert(0, tuple([row, x, [[y for y in xx] for xx in matrix_for_children], nqueens1]))
-            if row < n - 1:
-                if matrix_for_children[row + 1][x] == 0:
-                    q.insert(0, tuple([row + 1, x, [[y for y in xx] for xx in matrix_for_children], nqueens1]))
+    def find_possible_children(row, n, matrix, q, nqueens1):
+        propagation = 0
+        if row < n - 1:
+            for y in range(row, n):
+                for x in range(n):
+                    if y < n:
+                        if matrix[y][x] == 0:
+                            propagation = 1
+                            q.append(tuple([y, x, deepcopy(matrix), nqueens1]))
+                if propagation != 0:
+                    break
         return q
 
 
-    f = open("input.txt", "r")
-    f.readline()  # for the first line retrieval
-    n = int(f.readline().strip())  # Size of square board
-    p = int(f.readline().strip())  # Number of Queens
-
-    # n vs p check (TO BE DONE)
-    matrix1 = []
-    for x in range(n):  # generate state matrix
-        matrix1.append([int(x) if x == '0' else 'T' for x in f.readline().strip()])
-
-    q1 = deque()
-
-    for x in range(n):  # first column possible positions of Q
-        if matrix1[0][x] == 0:
-            q1.insert(0, tuple([0, x, deepcopy(matrix1), 0]))
-
-    nqueens1 = 0
-
-    def bfs(q, p):
+    def dfs(q, p):
         if p == 0:
             return p, deepcopy(q.pop()[2])
         queens_ret = 0
@@ -139,19 +114,43 @@ if __name__ == "__main__":
             if matrix[row][col] == 0:
                 matrix[row][col] = 'Q'
                 nqueens1 += 1
-
                 matrix = deepcopy(mark_conflicts(row, col, matrix))
-                print()
+                if nqueens1 == p:
+                    return nqueens1, deepcopy(matrix)
+                q = deepcopy(find_possible_children(row, n, matrix, q, nqueens1))
+                if row == n - 1:
+                    if nqueens1 < p:
+                        for x in range(n):
+                            for y in range(n):
+                                if matrix[x][y] == 0:
+                                    nqueens1 += 1
+                                    matrix[x][y] = 'Q'
+                                    matrix = deepcopy(mark_conflicts(x, y, matrix))
+                                    if nqueens1 == p:
+                                        return nqueens1, matrix
+                                    q = deepcopy(find_possible_children(row, n, matrix, q, nqueens1))
+                queens_ret = nqueens1
+            # print_output(matrix)
+            # print()
+            if queens_ret == p:
+                return queens_ret, matrix
+        return -1, matrix
 
+    def bfs(q, p):
+        if p == 0:
+            return p, deepcopy(q.pop()[2])
+        queens_ret = 0
+        while len(q) > 0:
+            row, col, matrix, nqueens1 = q.popleft()
+            if matrix[row][col] == 0:
+                matrix[row][col] = 'Q'
+                nqueens1 += 1
+                matrix = deepcopy(mark_conflicts(row, col, matrix))
                 if nqueens1 == p:
                     print("OK")
                     print_output(matrix)
                     return nqueens1, deepcopy(matrix)
-
                 q = find_possible_children(row, n, matrix, q, nqueens1)
-
-                print_matrix(matrix)
-
                 if len(q) == 0:
                     if nqueens1 < p:
                         for x in range(n):
@@ -160,30 +159,64 @@ if __name__ == "__main__":
                                     nqueens1 += 1
                                     matrix[x][y] = 'Q'
                                     matrix = deepcopy(mark_conflicts(x, y, matrix))
-
                                     if nqueens1 == p:
                                         print("OK")
                                         print_output(matrix)
                                         return nqueens1, matrix
                                     q = deepcopy(find_possible_children(row, n, matrix, q, nqueens1))
-
                 queens_ret = nqueens1
-
+            # print_output(matrix)
+            # print()
         if queens_ret == p:
             print("OK")
             print_output(matrix)
             return queens_ret, matrix
-
         return -1
 
+
+
+    f = open("input.txt", "r")
+    bfs_dfs = f.readline().strip()  # for the first line retrieval
+    print(bfs_dfs)
+    n = int(f.readline().strip())  # Size of square board
+    p = int(f.readline().strip())  # Number of Queens
+    matrix1 = []
+    for x in range(n):
+        matrix1.append([int(x) if x == '0' else 'T' for x in f.readline().strip()])
     start = time.time()
+    q1 = deque()
+    numberq = []
+    found = 0
+    for x in range(2):
+        matrix1 = [[i for i in j] for j in rotate(matrix1, 90)]
+        for xx in range(n):  # first column possible positions of Q
+            if matrix1[0][xx] == 0:
+                q1.append(tuple([0, xx, deepcopy(matrix1), 0]))
+        nqueens1 = 0
+        if bfs_dfs == "DFS":
+            ret_nq, matrix = dfs(q1, p)
+        else:
+            ret_nq, matrix = bfs(q1, p)
 
-    numberq, matrix = bfs(q1, p)
-    print()
-    print_matrix(matrix)
-    if numberq == -1:
-        print("FAIL")
+        if x == 0:
+            # print(x + 1)
+            matrix = deepcopy(rotate(matrix, 270))
 
+        elif x == 1:
+            # print(x + 1)
+            matrix = deepcopy(rotate(matrix, 180))
+
+        if ret_nq == p:
+            print("OK")
+            print_output(matrix)
+            found = 1
+            break
+            print()
+
+        numberq.append([ret_nq, matrix])
+
+    if found == 0:
+        if all([xx[0] == -1 for xx in numberq]):
+            print("FAIL")
     end = time.time()
-
     print("time : " + str(end - start))
