5eda528119eee40b1149909bf010de97def45777
vikashanumegowda <hanumego@usc.edu>
2017-09-30 17:28:26 -0700

    Figuring out the number of branches required

--- Assignment2/homework3.py
+++ Assignment2/homework3.py
@@ -1,4 +1,4 @@
-import collections
+from collections import OrderedDict
 import math
 from copy import deepcopy
 
@@ -11,106 +11,113 @@ def print_matrix(matrix):
 
 
 def calculate_connectivity(matrix, x, y, n):
+    # return x-coord, y-coord, sumvalue, matrix
+    if matrix[x][y][0] == '*':
+        return 0
     if x == y == 0 and n == 1:
-        return matrix[x][y][0], deepcopy(matrix)
+        return x, y, matrix[x][y][0], deepcopy(matrix)
     matrix[x][y][1] += 1
     if x == 0:
         if y == 0:
             # top-left
             if matrix[x][y + 1][0] == matrix[x][y][0] and matrix[x][y + 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y + 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y + 1, n)
                 matrix[x][y][2] += a
             if matrix[x + 1][y][0] == matrix[x][y][0] and matrix[x + 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x + 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x + 1, y, n)
                 matrix[x][y][2] += a
         elif y == n - 1:
             # top-right
             if matrix[x][y - 1][0] == matrix[x][y][0] and matrix[x][y - 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y - 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y - 1, n)
                 matrix[x][y][2] += a
             if matrix[x + 1][y][0] == matrix[x][y][0] and matrix[x + 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x + 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x + 1, y, n)
                 matrix[x][y][2] += a
         else:
             # top-row
             if matrix[x][y + 1][0] == matrix[x][y][0] and matrix[x][y + 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y + 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y + 1, n)
                 matrix[x][y][2] += a
             if matrix[x][y - 1][0] == matrix[x][y][0] and matrix[x][y - 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y - 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y - 1, n)
                 matrix[x][y][2] += a
             if matrix[x + 1][y][0] == matrix[x][y][0] and matrix[x + 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x + 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x + 1, y, n)
                 matrix[x][y][2] += a
     elif x == n - 1:
         if y == 0:
             # bottom-left
             if matrix[x][y + 1][0] == matrix[x][y][0] and matrix[x][y + 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y + 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y + 1, n)
                 matrix[x][y][2] += a
             if matrix[x - 1][y][0] == matrix[x][y][0] and matrix[x - 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x - 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x - 1, y, n)
                 matrix[x][y][2] += a
-
         elif y == n - 1:
             # bottom-right
             if matrix[x][y - 1][0] == matrix[x][y][0] and matrix[x][y - 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y - 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y - 1, n)
                 matrix[x][y][2] += a
             if matrix[x - 1][y][0] == matrix[x][y][0] and matrix[x - 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x - 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x - 1, y, n)
                 matrix[x][y][2] += a
         else:
             # bottom-row
             if matrix[x][y + 1][0] == matrix[x][y][0] and matrix[x][y + 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y + 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y + 1, n)
                 matrix[x][y][2] += a
             if matrix[x][y - 1][0] == matrix[x][y][0] and matrix[x][y - 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y - 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y - 1, n)
                 matrix[x][y][2] += a
             if matrix[x - 1][y][0] == matrix[x][y][0] and matrix[x - 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x - 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x - 1, y, n)
                 matrix[x][y][2] += a
     else:
         if y == 0:
             # middle-left
             if matrix[x][y + 1][0] == matrix[x][y][0] and matrix[x][y + 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y + 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y + 1, n)
                 matrix[x][y][2] += a
             if matrix[x - 1][y][0] == matrix[x][y][0] and matrix[x - 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x - 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x - 1, y, n)
                 matrix[x][y][2] += a
             if matrix[x + 1][y][0] == matrix[x][y][0] and matrix[x + 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x + 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x + 1, y, n)
                 matrix[x][y][2] += a
-
         elif y == n - 1:
             # middle-right
             if matrix[x][y - 1][0] == matrix[x][y][0] and matrix[x][y - 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y - 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y - 1, n)
                 matrix[x][y][2] += a
             if matrix[x - 1][y][0] == matrix[x][y][0] and matrix[x - 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x - 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x - 1, y, n)
                 matrix[x][y][2] += a
             if matrix[x + 1][y][0] == matrix[x][y][0] and matrix[x + 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x + 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x + 1, y, n)
                 matrix[x][y][2] += a
         else:
             # middle-middle :-p
             if matrix[x][y - 1][0] == matrix[x][y][0] and matrix[x][y - 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y - 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y - 1, n)
                 matrix[x][y][2] += a
             if matrix[x][y + 1][0] == matrix[x][y][0] and matrix[x][y + 1][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x, y + 1, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x, y + 1, n)
                 matrix[x][y][2] += a
             if matrix[x - 1][y][0] == matrix[x][y][0] and matrix[x - 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x - 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x - 1, y, n)
                 matrix[x][y][2] += a
             if matrix[x + 1][y][0] == matrix[x][y][0] and matrix[x + 1][y][1] == 0:
-                a, matrix = calculate_connectivity(matrix, x + 1, y, n)
+                r1, r2, a, matrix = calculate_connectivity(matrix, x + 1, y, n)
                 matrix[x][y][2] += a
 
-    return matrix[x][y][2], deepcopy(matrix)
+    return x, y, matrix[x][y][2], deepcopy(matrix)
+
+    def max_level():
+        pass
+
+    def min_level():
+        pass
 
 
 if __name__ == "__main__":
@@ -119,7 +126,9 @@ if __name__ == "__main__":
     n = int(f.readline().strip())
     p = int(f.readline().strip())
     t = float(f.readline().strip())
-    dict_fruit = {x: 0 for x in range(p)}
+    # dictfruit = {0 : [max_value, x-coord, y-coord]}
+    dict_fruit = OrderedDict()
+    dict_fruit = {x: [0, -1, -1] for x in range(p)}
     # print(dict_fruit)
     matrix = []
     for i in range(n):
@@ -127,9 +136,15 @@ if __name__ == "__main__":
         matrix.append(line)
     empty = deepcopy(matrix)
 
+    # calculate the max number of fruits that can be picked
     for x in range(n):
         for y in range(n):
-            z, matrix1 = calculate_connectivity(empty, x, y, n)
-            dict_fruit[empty[x][y][0]] = max(dict_fruit[empty[x][y][0]], z)
+            x1, y1, z, matrix1 = calculate_connectivity(empty, x, y, n)
+            if z > dict_fruit[empty[x][y][0]][0]:
+                dict_fruit[empty[x][y][0]][0] = z
+                dict_fruit[empty[x][y][0]][1] = x1
+                dict_fruit[empty[x][y][0]][2] = y1
+
+    dict_fruit = OrderedDict(reversed(sorted(dict_fruit.items(), key=lambda h: h[1][0])))
     print(dict_fruit)
     # print(z)
